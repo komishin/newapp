94f28fc5c1d6a37d62c2c601f35dc22270028fe2
古宮信二 <>
2025-10-16 13:29:00 +0900

    記事を削除できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
 
   root to: 'articles#index'
 
-  resources :articles, only: [:show, :new, :create, :edit, :update]
+  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
 
 end
