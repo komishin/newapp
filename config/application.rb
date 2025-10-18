579ca4af5a64532bd9a09d54716c59a4d03aff3e
古宮信二 <>
2025-10-18 15:33:57 +0900

    better errorsを導入

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -16,13 +16,13 @@ module Newapp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+     # Configuration for the application, engines, and railties goes here.
+     #
+     # These settings can be overridden in specific environments using the files
+     # in config/environments, which are processed later.
+     #
+     # config.time_zone = "Central Time (US & Canada)"
+     # config.eager_load_paths << Rails.root.join("extras")
      config.i18n.default_locale = :ja
   end
 end
