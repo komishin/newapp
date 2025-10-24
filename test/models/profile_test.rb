0fc8893a5d5544fc9ba19d8111fd4b64401b70f3
古宮信二 <>
2025-10-24 17:46:54 +0900

    ユーザーのプロフィールページを作成

---
+++ test/models/profile_test.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer          not null
+#  nickname     :string
+#  introduction :text
+#  gender       :integer
+#  birthday     :date
+#  subscribed   :boolean          default(FALSE)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
+
+require "test_helper"
+
+class ProfileTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
